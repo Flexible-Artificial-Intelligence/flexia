5e82bd970f69b194f8b45f490ba74539fd86f4c7
vad13irt <w13vadirt@gmail.com>
2022-07-20 13:16:28 +0300

    updated

--- flexia/trainer/enums.py
+++ flexia/trainer/enums.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 
+from prometheus_client import Enum
 from ..enums import ExplicitEnum
 from ..hook import States
 
@@ -27,5 +28,23 @@ class SchedulingStrategy(ExplicitEnum):
     STEP = "step"
 
 
-class TrainerStates(States):
-    pass
+class TrainerStates(Enum):
+    INIT_START = "on_init_start"
+    INIT_END = "on_init_end"
+    TRAINING_START = "on_training_start"
+    TRAINING_END = "on_training_end"
+    TRAINING_STEP_START = "on_training_step_start"
+    TRAINING_STEP_END = "on_training_step_end"
+    VALIDATION_START = "on_validation_start"
+    VALIDATION_END = "on_validation_end"
+    EPOCH_START = "on_epoch_start"
+    VALIDATION_STEP_START = "on_validation_step_start"
+    VALIDATION_STEP_END = "on_validation_step_end"
+    EPOCH_END = "on_epoch_end"
+    TRAINING_STOP = "on_training_stop"
+    CHECKPOINT_SAVE = "on_checkpoint_save"
+    EXCEPTION = "on_exception"
+    PREDICTION_START = "on_prediction_start"
+    PREDICTION_END = "on_prediction_end"
+    PREDICTION_STEP_START = "on_prediction_step_start"
+    PREDICTION_STEP_END = "on_prediction_step_end"

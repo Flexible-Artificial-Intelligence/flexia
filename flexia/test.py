a35e5f0a5fe65e98cf13f11a301f9afb612741cb
vad13irt <w13vadirt@gmail.com>
2022-08-05 17:06:32 +0300

    updated loggers, callbacks and accelerators

--- flexia/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("./")
-
-from flexia.accelerators.cpu_accelerator import CPUAccelerator
-from flexia.loggers.utils import format_accelerator_stats
-
-
-accelerator = CPUAccelerator(device="cpu:0", unit="MB")
-print(format_accelerator_stats(accelerator=accelerator))

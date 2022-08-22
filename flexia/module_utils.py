3e53eff92538bc8a34330450c06c7a9d68255b51
vad13irt <w13vadirt@gmail.com>
2022-08-22 13:25:33 +0300

    updated module_utils

--- flexia/module_utils.py
+++ flexia/module_utils.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Callable
 
 
 def freeze_module(module: nn.Module, verbose: bool = False) -> None:
@@ -27,21 +27,44 @@ def move_module_to_eval_mode(module: nn.Module, half_mode: bool = False) -> None
         module.half()
 
 
-def get_grouped_module_parameters(module: nn.Module, weight_decay: float = 0.01) -> List[Dict[str, Any]]:
-    model_parameters = list(module.named_parameters())
-    no_decay_layers = ("bias", "LayerNorm.bias", "LayerNorm.weight")
-    
-    decay_parameters = [p for n, p in model_parameters if not any(nd in n for nd in no_decay_layers)]
-    no_decay_parameters = [p for n, p in model_parameters if any(nd in n for nd in no_decay_layers)]
-
-    grouped_module_parameters = [
-        {"params": decay_parameters, "weight_decay": weight_decay},
-        {"params": no_decay_parameters, "weight_decay": 0.0},
-    ]
-
-    return grouped_module_parameters
-
-
 def save_module(module: nn.Module, path: str = "module.pt") -> None:
     module_state = module.state_dict()
     saved_state = torch.save(module_state, path)
+
+
+def replace_layer_with_other_layer(module: nn.Module, 
+                                   layer_instance: nn.Module,
+                                   other_layer_instance: nn.Module, 
+                                   get_parameters: Callable[[nn.Module], Dict[str, Any]] = lambda module: {}, 
+                                   load_state_dict: bool = False):
+    """
+    Recursively iterrates over all modules and submodules in the given module and replaces `layer_instance` module with `other_layer_instance` module.
+    The `other_layer_instance` parameters are given from `get_parameters` function.
+    Additionally, `replace_layer_with_other_layer` can load `state_dict` from `layer_instance` to newly initialized `other_layer_instance`.
+    """
+    
+    for submodule in module.modules():
+        for name, layer in submodule.named_children():
+            layer_children = layer.children()
+            num_layer_children = len(list(layer_children))
+            
+            # Replaces the layer's childrens and replaces `layer_instance` module with `other_layer_instance` module.
+            if num_layer_children > 0:
+                replace_layer_with_other_layer(module=layer, 
+                                               layer_instance=layer_instance,
+                                               other_layer_instance=other_layer_instance, 
+                                               get_parameters=get_parameters, 
+                                               load_state_dict=load_state_dict)
+                
+            # Replaces `layer_instance` module with `other_layer_instance` module.
+            if isinstance(layer, layer_instance):
+                new_layer_parameters = get_parameters(layer)
+                new_layer = other_layer_instance(**new_layer_parameters)
+                
+                # Loads state dictionary from `layer` (i.e `layer_instance`) for newly initialized `other_layer_instance`.
+                if load_state_dict:
+                    layer_state_dict = layer.state_dict()
+                    new_layer.load_state_dict(layer_state_dict)
+                    
+                # Replaces `name` in `submodule` with `new_layer` (i.e newly initialized `other_layer_instnce`).
+                setattr(submodule, name, new_layer)
